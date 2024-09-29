ec6505f7e48df4d19c15934d7e0a96bc06849290
makadown <maseflores79@gmail.com>
2024-09-29 16:40:01 -0400

    adding entity classes

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace AuctionService.Entities;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+
+    /// <summary>
+    /// username from claim
+    /// </summary>
+    public string Seller { get; set; } = string.Empty;
+    /// <summary>
+    /// username of winner
+    /// </summary>
+    public string Winner { get; set; }
+
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    public Status Status { get; set; } = Status.Live;
+    public Item Item { get; set; } 
+}
