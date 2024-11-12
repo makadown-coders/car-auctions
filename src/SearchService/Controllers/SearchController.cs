47a64d0434126f1d9b6bdb498d68e4d1875460f8
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-11-12 12:38:37 -0800

    paginando resultados

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController: ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
+                int pageNumber =1, int pageSize = 4)
+    {
+        var query = DB.PagedSearch<Item>();
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrWhiteSpace(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
+
+        var result = await query.ExecuteAsync();
+        return Ok(new {
+            results = result.Results,
+            pageNumber = pageNumber,
+            pageSize = pageSize,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
+    }
+}
