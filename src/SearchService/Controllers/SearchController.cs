faf56179d66c4593e8772aa869847f62b9e356ef
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-11-12 15:55:31 -0800

    microservicio de busqueda completado

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -10,25 +11,48 @@ namespace SearchService.Controllers;
 public class SearchController: ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm,
-                int pageNumber =1, int pageSize = 4)
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]SearchParams searchParams)
     {
-        var query = DB.PagedSearch<Item>();
-        query.Sort(x => x.Ascending(a => a.Make));
+        var query = DB.PagedSearch<Item, Item>();
 
-        if (!string.IsNullOrWhiteSpace(searchTerm))
+        if (!string.IsNullOrWhiteSpace(searchParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
+        
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        query = searchParams.OrderBy switch
+        {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => 
+                        x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
         }
 
-        query.PageNumber(pageNumber);
-        query.PageSize(pageSize);
+        if (!string.IsNullOrEmpty(searchParams.Winner))
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
         return Ok(new {
             results = result.Results,
-            pageNumber = pageNumber,
-            pageSize = pageSize,
+            pageNumber = searchParams.PageNumber,
+            pageSize = searchParams.PageSize,
             pageCount = result.PageCount,
             totalCount = result.TotalCount
         });
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,13 +1,20 @@
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services
+    .AddHttpClient<AuctionSvcHttpClient>()
+    .AddPolicyHandler(GetPolicy());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
@@ -19,13 +26,23 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DBInitializer.InitDb(app);
-}
-catch(Exception ex)
-{
-    Console.WriteLine(ex);
-}
+    // haciendo el endpoint resiliente
+    try
+    {
+        await DBInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+});
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() =>
+    HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
